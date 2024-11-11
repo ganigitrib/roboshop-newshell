8b48a3ac01d16928536da8bdc70a7cd230c11bac
ganeshkaibada1 <ganeshkaibada7036@gamil.com>
2024-11-11 21:14:09 +0530

    print script

--- Dispatch.sh
+++ Dispatch.sh
@@ -1,31 +1,34 @@
-Color="\e[35m
-no_color=\e[0m"
-echo -e "$color copy Dispatch service file $no_color"
+# Define colors
+color="\e[35m"          # Missing closing quote fixed
+no_color="\e[0m"        # Move this to a new line
+
+# Printing with colors
+echo -e "${color}Copy Dispatch service file${no_color}"
 cp Dispatch.service /etc/systemd/system/dispatch.service
 
-echo -e "$Color install Golang $no_color"
- dnf install golang -y
-echo -e "$color add Application User $no_color"
- useradd roboshop
+echo -e "${color}Install Golang${no_color}"
+dnf install golang -y
 
- echo -e "$Color Create Application Directory $no_color"
- rm -rf /app
- mkdir /app
+echo -e "${color}Add Application User${no_color}"
+useradd roboshop
 
+echo -e "${color}Create Application Directory${no_color}"
+rm -rf /app
+mkdir /app
 
-echo -e "$Color Download Application Content $no_color"
- curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
- cd /app
+echo -e "${color}Download Application Content${no_color}"
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
+cd /app
 
- echo -e "$Color Extract Application Content $no_color"
- unzip /tmp/dispatch.zip
+echo -e "${color}Extract Application Content${no_color}"
+unzip /tmp/dispatch.zip
 
-echo -e "$Color Copy Download Application Dependencies $no_color"
+echo -e "${color}Copy Download Application Dependencies${no_color}"
 go mod init dispatch
 go get
 go build
 
-echo -e "$Color Start Application Dependencies $no_color"
+echo -e "${color}Start Application Dependencies${no_color}"
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl restart dispatch
