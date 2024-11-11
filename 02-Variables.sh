bef6c238554ab0e28dcc91ed6c34c9ba007d1df1
ganeshkaibada1 <ganeshkaibada7036@gamil.com>
2024-11-11 20:15:30 +0530

    print script

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -1,15 +1,3 @@
-# if we assign a name to a set of data is called as variable
-# Variable=Data
 
-# To access Variable we use $variable or $(variable)
-
-Trainer=Steve
-
-echo DevOps Trainer - $Trainer
-echo Aws Trainer - $Trainer
-
-
-
-# Trainer=John
 
 
