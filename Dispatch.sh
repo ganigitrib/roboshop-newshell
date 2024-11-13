aff7af1d54b41ca0076567d4cce9dd592079e4d8
ganeshkaibada1 <ganeshkaibada7036@gamil.com>
2024-11-13 20:28:49 +0530

    common script

--- Dispatch.sh
+++ Dispatch.sh
@@ -5,11 +5,12 @@ COLOR='\e[1;32m'  # Green color for messages
 NO_COLOR='\e[0m'  # Reset color to default
 app_name=dispatch
 echo "Sourcing common.sh"
-source /home/ec2-user/roboshop-newshell/common.sh
+source /home/ec2-user/roboshop-newshell/Common.sh
 
 echo -e "${COLOR}Copy Dispatch service file${NO_COLOR}"
 cp Dispatch.service /etc/systemd/system/dispatch.service
 echo $?
+
 echo -e "${COLOR}Install Go Language${NO_COLOR}"
 dnf install golang -y
 echo $?
@@ -21,6 +22,7 @@ go mod init dispatch
 go get
 go build
 echo $?
+
 echo -e "${COLOR}Start Application Service${NO_COLOR}"
 systemctl daemon-reload
 systemctl enable dispatch
