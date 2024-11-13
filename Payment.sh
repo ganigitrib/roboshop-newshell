f5d6ca695c0ce0c4d04b8c9f2ad255ba4378833e
ganeshkaibada1 <ganeshkaibada7036@gamil.com>
2024-11-14 01:53:57 +0530

    common script

--- Payment.sh
+++ Payment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Define color variables
+# shellcheck disable=SC2034
 COLOR='\e[1;32m'  # Green color for messages
 NO_COLOR='\e[0m'  # Reset color to default
 app_name=Payment
@@ -9,6 +10,7 @@ app_name=Payment
 echo "Sourcing common.sh"
 source /home/ec2-user/roboshop-newshell/Common.sh
 
+# shellcheck disable=SC2154
 echo -e "$color Copy Payment Service file $no_color"
 cp Payment.service /etc/systemd/system/payment.service
 echo $?  # Check if the copy was successful
@@ -17,6 +19,7 @@ app_prerequisites
 echo $?  # Check if the prerequisites installation was successful
 
 echo -e "$color Download Application Dependencies $no_color"
+Cd /app
 pip3 install -r requirements.txt
 echo $?  # Check if the dependencies installation was successful
 
