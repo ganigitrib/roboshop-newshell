9a9852eebf950347aaaca9d44702874829a53cd3
ganeshkaibada1 <ganeshkaibada7036@gamil.com>
2024-11-14 16:34:39 +0530

    common script

--- Common.sh
+++ Common.sh
@@ -3,26 +3,27 @@
 # Define color variables
 COLOR="\e[32m"          # Green color
 NO_COLOR="\e[0m"        # Reset color
-rm -f $log_file
+
+# Define log file
+log_file="/tmp/app_install.log"
+rm -f "$log_file"       # Remove log file if it exists to start fresh
 
 app_prerequisites() {
     echo -e "${COLOR}Create Application User${NO_COLOR}"
-     useradd roboshop &>>$log_file
-     echo $?
+    useradd roboshop &>>"$log_file"
+    echo $?
 
     echo -e "${COLOR}Create Application Directory${NO_COLOR}"
-    rm -rf /app &>>$log_file
-    mkdir /app &>>$log_file
+    rm -rf /app &>>"$log_file"
+    mkdir /app &>>"$log_file"
     echo $?
 
     echo -e "${COLOR}Download Application content${NO_COLOR}"
-    curl -L -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}.zip &>>$log_file
+    curl -L -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}.zip &>>"$log_file"
     echo $?
 
-     cd /app
-
     echo -e "${COLOR}Extract Application content${NO_COLOR}"
-    cd /app
-    unzip /tmp/${app_name}.zip &>>$log_file
+    cd /app || exit 1  # Ensure we’re in /app; exit with error if cd fails
+    unzip /tmp/${app_name}.zip &>>"$log_file"
     echo $?
 }
--- Dispatch.sh
+++ Dispatch.sh
@@ -3,28 +3,37 @@
 # Define color variables
 COLOR='\e[1;32m'  # Green color for messages
 NO_COLOR='\e[0m'  # Reset color to default
-app_name=dispatch
+
+# Define application name and log file path
+app_name="dispatch"
+log_file="/tmp/dispatch_install.log"  # Ensure log_file is assigned
+
 echo "Sourcing common.sh"
 source /home/ec2-user/roboshop-newshell/Common.sh
 
+# Copy the Dispatch service file to systemd directory
 echo -e "${COLOR}Copy Dispatch service file${NO_COLOR}"
-cp Dispatch.service /etc/systemd/system/dispatch.service &>>$log_file
+cp Dispatch.service /etc/systemd/system/dispatch.service &>>"$log_file"
 echo $?
 
+# Install Go Language
 echo -e "${COLOR}Install Go Language${NO_COLOR}"
-dnf install golang -y &>>$log_file
+dnf install golang -y &>>"$log_file"
 echo $?
 
+# Call the app_prerequisites function from Common.sh
 app_prerequisites
 
+# Build the application
 echo -e "${COLOR}Build Application${NO_COLOR}"
-go mod init dispatch
-go get &>>$log_file
-go build &>>$log_file
+go mod init "$app_name" &>>"$log_file"
+go get &>>"$log_file"
+go build &>>"$log_file"
 echo $?
 
+# Start and enable the Dispatch service
 echo -e "${COLOR}Start Application Service${NO_COLOR}"
-systemctl daemon-reload &>>$log_file
-systemctl enable dispatch &>>$log_file
-systemctl restart dispatch &>>$log_file
+systemctl daemon-reload &>>"$log_file"
+systemctl enable dispatch &>>"$log_file"
+systemctl restart dispatch &>>"$log_file"
 echo $?
