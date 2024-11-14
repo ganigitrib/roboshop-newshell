59f5d37341a1f800a30d7bb3ad8508ffd9470003
ganeshkaibada1 <ganeshkaibada7036@gamil.com>
2024-11-14 16:21:17 +0530

    common script

--- Common.sh
+++ Common.sh
@@ -3,26 +3,26 @@
 # Define color variables
 COLOR="\e[32m"          # Green color
 NO_COLOR="\e[0m"        # Reset color
-
+rm -f $log_file
 
 app_prerequisites() {
     echo -e "${COLOR}Create Application User${NO_COLOR}"
-     useradd roboshop
+     useradd roboshop &>>$log_file
      echo $?
 
     echo -e "${COLOR}Create Application Directory${NO_COLOR}"
-    rm -rf /app
-    mkdir /app
+    rm -rf /app &>>$log_file
+    mkdir /app &>>$log_file
     echo $?
 
     echo -e "${COLOR}Download Application content${NO_COLOR}"
-    curl -L -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}.zip
+    curl -L -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}.zip &>>$log_file
     echo $?
 
      cd /app
 
     echo -e "${COLOR}Extract Application content${NO_COLOR}"
     cd /app
-    unzip /tmp/${app_name}.zip
+    unzip /tmp/${app_name}.zip &>>$log_file
     echo $?
 }
--- Dispatch.sh
+++ Dispatch.sh
@@ -8,23 +8,23 @@ echo "Sourcing common.sh"
 source /home/ec2-user/roboshop-newshell/Common.sh
 
 echo -e "${COLOR}Copy Dispatch service file${NO_COLOR}"
-cp Dispatch.service /etc/systemd/system/dispatch.service
+cp Dispatch.service /etc/systemd/system/dispatch.service &>>$log_file
 echo $?
 
 echo -e "${COLOR}Install Go Language${NO_COLOR}"
-dnf install golang -y
+dnf install golang -y &>>$log_file
 echo $?
 
 app_prerequisites
 
 echo -e "${COLOR}Build Application${NO_COLOR}"
 go mod init dispatch
-go get
-go build
+go get &>>$log_file
+go build &>>$log_file
 echo $?
 
 echo -e "${COLOR}Start Application Service${NO_COLOR}"
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl restart dispatch
+systemctl daemon-reload &>>$log_file
+systemctl enable dispatch &>>$log_file
+systemctl restart dispatch &>>$log_file
 echo $?
